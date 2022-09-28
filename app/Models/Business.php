c5e2a394a86b53c17d55675866590ebcc5c8c3de
RicarbonatoNA <gonzalez.ricardo.1bm@gmail.com>
2022-09-28 12:06:03 -0500

    se integró Stripe

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -12,7 +12,7 @@ class Business extends Model
     use HasFactory;
     protected $table='business';
     protected $fillable =['id', 'name', 'identificador_fiscal',
-    'direccion_facturacion', 'telefono', 'logos', 'notas', 'user_id',];
+    'direccion_facturacion', 'telefono', 'logo', 'notas', 'user_id',];
     public $timetamps = false;
 }
 
